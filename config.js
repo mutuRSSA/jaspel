60c6bf736b92d0d96357d82cedf25322db67704e
mutuRSSA <kmkp.rssa@gmail.com>
2026-06-16 10:29:07 +0700

    Change upload behavior

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
  * =========================================================================
  */
 
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbw5tzVro0b-YP0CT5UPR45a21Ir_P2f39KW_nv774MZVIxvFnPKWEWeLIOfKG_qay_K/exec"; 
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyFkmkv84wtkBnu9S2Is6z5RNzSSTh__WYhH3Cu-zaYuIMUBswlCctxLGL0aNwO2C06/exec"; 
 
 function renderNavbar() {
     let currentRole = localStorage.getItem("userRole") || "operator";
